d71096046f256b1f377549c9ce91f492ab337d8b
shreya1313 <50258174+shreya1313@users.noreply.github.com>
2023-04-07 22:05:36 -0400

    update model 14_4

--- models/resnet_14_4.py
+++ models/resnet_14_4.py
@@ -42,7 +42,7 @@ class ResNet(nn.Module):
         self.layer1 = self.make_layer(block, 16, layers[0])
         self.layer2 = self.make_layer(block, 32, layers[1], 2)
         self.layer3 = self.make_layer(block, 64, layers[2], 2)
-        self.layer4 = self.make_layer(block, 512, layers[2])
+        self.layer4 = self.make_layer(block, 512, layers[3])
         self.avg_pool = nn.AvgPool2d(8)
         self.fc = nn.Linear(512, num_classes)
 
